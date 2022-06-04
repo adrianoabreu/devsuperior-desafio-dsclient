bbe64d9eb3a7d24da7b30d342697d076403164f6
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-06-04 20:37:19 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -3,10 +3,20 @@ package com.devsuperior.dsclient.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long    id;
 	private String  name;
 	private String  cpf;
--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -4,28 +4,27 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
-	private Instant date;
+	@Autowired
+	private ClientService service;
 	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();		
-		list.add(new Client(1L,"Adriano","13448701718",9000.00,date,0));
-		list.add(new Client(2L,"Andre","13448702790",3000.00,date,0));
-		list.add(new Client(3L,"Vera","12345678",2000.00,date,1));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
 	
 	
 }
